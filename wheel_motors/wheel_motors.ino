ce0e095ac7a4297d081016a39f5bf817ffdc593e
jah262 <jah262@cam.ac.uk>
2020-10-22 12:27:29 +0100

    WhEeL

--- wheel_motors/wheel_motors.ino
+++ wheel_motors/wheel_motors.ino
@@ -6,12 +6,14 @@
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); //creates the adafruit object
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1); //request the left wheel motor - should be on whatever number is inside getMotor()
+Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
 
 
 void setup() {
   // put your setup code here, to run once:
 
-  AFMS.begin() //activates the adafruit motor
+  AFMS.begin(); //activates the adafruit motor
+  Serial.begin(9600);
 
 }
 
@@ -20,6 +22,11 @@ void loop() {
 
   myMotor->setSpeed(120); //sets the speed in 
   myMotor ->run(FORWARD); //activates the motor to move forwards;
+
+  myMotor2->setSpeed(120); //sets the speed in 
+  myMotor2 ->run(FORWARD); //activates the motor to move forwards;
+
+  Serial.println("Hello");
   
   
 
