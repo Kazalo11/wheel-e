4ed91dae26c45d869ecfa8f8b302b43afad1c0b5
jah262 <jah262@cam.ac.uk>
2020-10-27 16:22:00 +0000

    Trying to fix broken colour code, still need average function

--- colour_sensor/colour_sensor.ino
+++ colour_sensor/colour_sensor.ino
@@ -6,8 +6,8 @@ const int RedLED = 13; //number of RedLED
 const int BlueLED = 12; //number of BlueLED
 const int GreenLED = 11; //number of GreenLED
 const int LDR = A0; // number pin for ColourSensor
-int BrightnessRed = [];
-int BrightnessBlue = []; // 2 empty arrays which will store all the LDR values from reading from colour sensor
+int BrightnessRed[] = {};
+int BrightnessBlue[] = {}; // 2 empty arrays which will store all the LDR values from reading from colour sensor
 
 int servoPin = 9; //pin where the servo will be attach
 Servo myservo; //creates a variable of the Servo object
@@ -22,8 +22,9 @@ void setup() {
   pinMode(GreenLED, OUTPUT);
   //sets the RedLED, GreenLED and BlueLED to output
 
-  myservo.attach(servoPin) //links the servo variable to the pin which will control the action
-  
+  myservo.attach(servoPin); //links the servo variable to the pin which will control the action
+  Serial.begin(9600);
+  Serial.println("Started");
 }
 
 void loop() {
@@ -52,13 +53,20 @@ void ripe_or_unripe() {
   digitalWrite(BlueLED,LOW); //turn blue led off
   float averageRed = average(BrightnessRed);
   float averageBlue = average(BrightnessBlue); //works out the average of each of the brightnesses
+  Serial.print("Average red brightness: ");
+  Serial.println(averageRed);
+  Serial.print("Average blue brightness: ");
+  Serial.println(averageBlue);
+  
+  
+  
 
   if (averageRed < averageBlue) { //more red light returned hence bottom must be blue
     StartTime = millis();
     while(millis() - StartTime <= 5500) {
       digitalWrite(GreenLED, HIGH);
     }
-    servo.write(90);
+    //Servo.write(90);
     //open door using servo motor
     //move forward collect fruit then close door
   }
@@ -73,11 +81,11 @@ void ripe_or_unripe() {
 }
 
 
-float average(int arr) { //finds the average of an array of numbers
+float average(int * array) { //finds the average of an array of numbers
   long sum = 0;
-  int len = sizeof(arr)/sizeof(arr[0]); // number of elements in an array
-  for (int j = 0; j < len ; i++) {
-    sum+= arr[j];
+  int len = sizeof(array)/sizeof(array[0]); // number of elements in an array
+  for (int j = 0; j < len ; j++) {
+    sum+= array[j];
   }
   return(float(sum/len));
 }
